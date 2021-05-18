80e9a7ad28c928efef089f3e6dc63e5d59aadbbc
ALLAHNANA <usmanallahnana@gmail.com>
2021-05-18 15:07:47 +0100

    Imlementing data types in js

---
+++ assignment.js
@@ -0,0 +1,28 @@
+var q;
+var p;
+var r;
+//STRING IMPLEMENTATION
+q = "Allahnana"
+console.log(q);
+console.log (typeof(q));
+//INTEGER IMPLEMENTATION
+q = 58;
+console.log(q);
+console.log (typeof(q));
+
+//FLOAT IMPLEMENTATION
+q = 1.0
+console.log ((q).toFixed(4));
+console.log (typeof(q));
+
+//BOOLEAN IMPLEMENTATION
+q = 15;
+p = 12;
+r = p > q;
+console.log(r);
+console.log (typeof(r));
+
+
+
+
+
